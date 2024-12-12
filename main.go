0af1e9f6ddc5b7dd4392afeda36908a1bd2033b5
mekonger <m3kong.dev@gmail.com>
2024-12-12 09:05:47 +0700

    Update the flow of input question

--- main.go
+++ main.go
@@ -1,25 +1,58 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"github.com/PullRequestInc/go-gpt3"
 	"github.com/joho/godotenv"
+	"log"
 	"os"
+	"strconv"
 )
 
+const (
+	ChatGPT35TurboInstructEngine = "gpt-3.5-turbo-instruct"
+)
+
+func makeRequest(question string) gpt3.CompletionRequest {
+	maxToken, _ := strconv.Atoi(os.Getenv("MAX_TOKEN"))
+	temperature, _ := strconv.ParseFloat(os.Getenv("TEMPERATURE"), 32)
+
+	questions := []string{question}
+	return gpt3.CompletionRequest{
+		Prompt:      questions,
+		MaxTokens:   gpt3.IntPtr(maxToken),
+		Temperature: gpt3.Float32Ptr(float32(temperature)),
+	}
+}
+
+func complete(ctx context.Context, client gpt3.Client, question string) {
+
+	request := makeRequest(question)
+	resp, _ := client.Completion(ctx, request)
+
+	fmt.Println(resp.Choices[0].Text)
+}
+
 func main() {
-	godotenv.Load()
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("Can't load config, error: %v\n", err)
+		return
+	}
+
 	apiKey := os.Getenv("API_KEY")
 	ctx := context.Background()
 	client := gpt3.NewClient(apiKey)
-	request := gpt3.CompletionRequest{
-		Prompt: []string{"How many coffees should I drink per day?"},
-	}
-	resp, err := client.CompletionWithEngine(ctx, "gpt-3.5-turbo-instruct", request)
-	if err != nil {
-		fmt.Printf("%s\n", err)
-	} else {
-		fmt.Printf("Answer: %s\n", resp.Choices[0].Text)
+
+	for {
+		fmt.Print("\n\n> ")
+		reader := bufio.NewReader(os.Stdin)
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		complete(ctx, client, line)
 	}
 }
